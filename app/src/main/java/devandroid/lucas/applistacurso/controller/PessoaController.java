26fc02210f0fb4b6b1ea63ea5f5bab4364d10b2d
Lucas <trabalholucas93@gmail.com>
2024-05-26 16:25:03 -0300

    Refatorada a Ckasse PessoaController.

--- app/src/main/java/devandroid/lucas/applistacurso/controller/PessoaController.java
+++ app/src/main/java/devandroid/lucas/applistacurso/controller/PessoaController.java
@@ -1,12 +1,25 @@
 package devandroid.lucas.applistacurso.controller;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import devandroid.lucas.applistacurso.model.Pessoa;
+import devandroid.lucas.applistacurso.view.MainActivity;
 
 public class PessoaController {
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+    public static final String NOME_PREFERENCES = "pref_listavip";
+
+
+    public PessoaController(MainActivity mainActivity) {
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+
+        listaVip = preferences.edit();
+    }
+
     @NonNull
     @Override
     public String toString() {
@@ -14,8 +27,33 @@ public class PessoaController {
         return super.toString();
     }
 
+    public void criarSharedPreferences() {
+
+    }
+
+
     public void salvar(Pessoa pessoa) {
         Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
 
+        listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVip.putString("sobreNome", pessoa.getSobreNome());
+        listaVip.putString("nomeCurso", pessoa.getCursoDesejado());
+        listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVip.apply();
     }
+
+    public Pessoa buscar(Pessoa pessoa) {
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", ""));
+        pessoa.setSobreNome(preferences.getString("sobreNome", ""));
+        pessoa.setCursoDesejado(preferences.getString("nomeCurso", ""));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", ""));
+
+        return pessoa;
+    }
+
+    public void limpar() {
+        listaVip.clear();
+        listaVip.apply();
+    }
+
 }

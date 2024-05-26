6e90f096ef1b06380e7ecb4520e60c4afe9b0680
Lucas <trabalholucas93@gmail.com>
2024-05-26 13:46:28 -0300

    SharedPreferences Limpara dados Salvos

--- app/src/main/java/devandroid/lucas/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/lucas/applistacurso/view/MainActivity.java
@@ -17,6 +17,8 @@ import devandroid.lucas.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+
     public static final String NOME_PREFERENCES = "pref_listavip";
     PessoaController controller;
     Pessoa pessoa;
@@ -37,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preferences = getSharedPreferences(NOME_PREFERENCES,0);
-        SharedPreferences.Editor listaVip = preferences.edit();
+        listaVip = preferences.edit();
 
         controller = new PessoaController();
         controller.toString();
@@ -67,6 +69,10 @@ public class MainActivity extends AppCompatActivity {
             editSobreNomeAluno.setText("");
             editNomeDoCurso.setText("");
             editTelefoneContato.setText("");
+
+            listaVip.clear();
+            listaVip.apply();
+
         });
 
         btnFinalizar.setOnClickListener(v -> {
